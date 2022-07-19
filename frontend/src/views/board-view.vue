ebf8a4d7da25cf48e4b9b87826ee37c9d0b61daf
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-07-19 23:22:55 +0300

    try dd

--- frontend/src/views/board-view.vue
+++ frontend/src/views/board-view.vue
@@ -1,12 +1,6 @@
 <template>
     <div v-if="board">
-        <!-- <Container @drop="onDrop"> -->
-        <!-- <Draggable v-model="board.groups"> -->
-        <!-- <board-group v-for="(group, idx) in board.groups" :group="group" :key="group.id" :idx="idx" /> -->
-
-        <!-- </Draggable> -->
-        <!-- </Container> -->
-        <Container group-name="cols" tag="div" orientation="horizontal">
+        <!-- <Container group-name="cols" tag="div" orientation="horizontal">
             <Draggable v-for="(group, idx) in board.groups" :group="group" :key="group.id" :idx="idx">
                 <div>
                     <span>{{ group.title }}</span>
@@ -29,6 +23,41 @@
                     <board-group v-for="task in group.tasks" :task=task />
                 </Container>
             </Draggable>
+        </Container> -->
+        <Container class="h-full flex overflow-x-auto gap-8  p-8" group-name="cols" tag="div" orientation="horizontal"
+            @drop="onColumnDrop($event)">
+            <Draggable class="bg-gray-200 dark:bg-gray-700 rounded-lg h-full w-96 flex-shrink-0 shadow-xl"
+                v-for="(group, idx) in board.groups" :group="group" :key="group.id" :idx="idx">
+                <div class="h-full flex flex-col">
+
+                    <!-- header-->
+                    <div class="cursor-move rounded-t-lg p-4 space-x-4 bg-primary text-white flex space-x-2">
+                        <span>{{ group.title }}</span>
+                    </div>
+
+                    <!-- column -->
+                    <Container class="flex-grow overflow-y-auto overflow-x-hidden" orientation="vertical"
+                        group-name="group-items"
+                        :shouldAcceptDrop="(e, payload) => (e.groupName === 'group-items' && !payload.loading)"
+                        :get-child-payload="getCardPayload(group.id)" :drop-placeholder="{
+                            className:
+                                `bg-primary bg-opacity-20  
+                                                                                    border-dotted border-2 
+                                                                                    border-primary rounded-lg mx-4 my-2`,
+                            animationDuration: '200',
+                            showOnTop: true
+                        }" drag-class="bg-primary dark:bg-primary 
+            border-2 border-primary-hover text-white 
+            transition duration-100 ease-in z-50
+            transform rotate-6 scale-110" drop-class="transition duration-100 
+            ease-in z-50 transform 
+            -rotate-2 scale-90" @drop="(e) => onCardDrop(group.id, e)">
+
+                        <!-- Items -->
+                        <kanbanItem v-for="task in group.tasks" :key="task.id" :task="task" />
+                    </Container>
+                </div>
+            </Draggable>
         </Container>
     </div>
 </template>
@@ -37,12 +66,14 @@
 import { boardService } from '../service/board-service';
 import { Container, Draggable } from "vue3-smooth-dnd";
 import boardGroup from '../components/board-group.vue'
+import kanbanItem from '../components/kanban-Item.vue'
 
 export default {
     name: 'board-view',
     components: {
         Container,
         Draggable,
+        kanbanItem,
         boardGroup
     },
 
@@ -57,7 +88,6 @@ export default {
     },
     methods: {
         applyDrag(arr, dragResult) {
-            console.log(': im the problem 60')
             const { removedIndex, addedIndex, payload } = dragResult;
 
             if (removedIndex === null && addedIndex === null) return arr;
@@ -79,21 +109,26 @@ export default {
             return kanban ? kanban.offsetHeight - 122 : 0;
         },
         onColumnDrop(dropResult) {
-            console.log(': im the problem 82')
+            console.log('dropResult: ', dropResult)
             const board = Object.assign({}, this.board)
             board.groups = this.applyDrag(board.groups, dropResult)
             this.board = board
         },
-        onCardDrop(columnId, dropResult) {
+        async onCardDrop(groupId, dropResult) {
             console.log(': im the problem 88')
 
             // check if element where ADDED or REMOVED in current collumn
             if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
-
+                console.log(': afterchek')
                 const board = Object.assign({}, this.board)
-                const column = board.groups.filter(p => p.id === columnId)[0]
-                const itemIndex = board.groups.indexOf(column)
-                const newColumn = Object.assign({}, column)
+                console.log('board: ' , board)
+                const group = await board.groups.filter(p => p.id === groupId)[0]
+                console.log('board.groups: ' , board.groups)
+                console.log('group: ' , group.id)
+                const itemIndex =await  board.groups.indexOf(group.id)
+                console.log('itemIndex: ' , itemIndex)
+                const newColumn = Object.assign({}, group)
+                console.log('newColumn: ' , newColumn)
 
                 // check if element was ADDED in current column
                 if ((dropResult.removedIndex == null && dropResult.addedIndex >= 0)) {
