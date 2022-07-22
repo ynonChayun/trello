1fcfd883da538a9fb3a0b76fa5fbec8c52b228e3
Amit Sharafi <amitsharafi9@gmail.com>
2022-07-22 17:57:55 +0300

    sa

--- frontend/src/views/home-view.vue
+++ frontend/src/views/home-view.vue
@@ -1,15 +1,27 @@
 <template>
-  <section class="home-page">
-    <h1>Home Page</h1>
-    <router-link to="/board">go to board list</router-link>
+  <section class="home-view">
+    <div class="hero">
+      <div class="hero-sign-up">
+        <h1>Trello helps teams move work forward.</h1>
+        <p>
+          Collaborate, manage projects, and reach new productivity peaks. From
+          high rises to the home office, the way your team works is
+          unique—accomplish it all with Trello.
+        </p>
+        <button @click="this.$router.push('/board')">Get started</button>
+      </div>
+      <div class="hero-img">
+        <img src="../imgs/hero.webp" />
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
-  created(){
-       this.$store.dispatch({ type: "loadBoards" })
-  }
+  created() {
+    this.$store.dispatch({ type: 'loadBoards' })
+  },
 }
 </script>
 
